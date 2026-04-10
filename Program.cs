09954d0cbb83799961ba7d7bd52155f7f3ef26ba
kledezma <kledezma@benchly.com>
2026-04-10 13:11:54 -0600

    Programs missing code

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
-using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApi;
 
+// Servicios
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddDbContext<Datos.ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection2")));
 
+builder.Services.AddTransient<ServicioTransient>();
+builder.Services.AddScoped<ServicioScope>();
+builder.Services.AddSingleton<ServicioSingleton>();
+builder.Services.AddSingleton<IRepositorioValores, RepositorioValoresOracle>();
+
+
+
 var app = builder.Build();
 
-// Servicios
 
 //app.MapGet("/", () => "Hello World!");
 
 // Middlewares
+app.UseLogueaPeticion();
+app.UseBloqueaPeticion();
 app.MapControllers();
 app.Run();
